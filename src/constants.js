e77f883ccbb333b5b8b12b5ac80e4f13ac03eaa2
Akash Panigrahi <akashpanigrahi@gofynd.com>
2024-02-01 13:30:14 +0530

    add utm params

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,15 @@
+const utmQueryParams = "utm_source=photoshop&utm_medium=plugin&utm_campaign=upscalemedia";
+
 export const constants = {
     urls: {
-        redirectToAppsPage:
-            "https://console.pixelbin.io/choose-org?redirectTo=settings/apps",
-        redirectToDashboardPage:
-            "https://console.pixelbin.io/choose-org?redirectTo=dashboard",
-        redirectToPricingPage:
-            "https://console.pixelbin.io/choose-org?redirectTo=settings/billing/pricing",
+        redirectToAppsPage: `https://console.pixelbin.io/choose-org?redirectTo=settings/apps&${utmQueryParams}`,
+        redirectToDashboardPage: `https://console.pixelbin.io/choose-org?redirectTo=dashboard&${utmQueryParams}`,
+        redirectToPricingPage: `https://console.pixelbin.io/choose-org?redirectTo=settings/billing/pricing&${utmQueryParams}`,
 
-        orgPricingPage:
-            "https://console.pixelbin.io/organization/:orgId/settings/billing/pricing",
+        orgPricingPage: `https://console.pixelbin.io/organization/:orgId/settings/billing/pricing?${utmQueryParams}`,
 
-        pluginHomePage: "https://www.upscale.media",
-        pluginDoc:
-            "https://www.pixelbin.io/docs/integrations/photoshop/upscale.media/",
+        pluginHomePage: `https://www.upscale.media?${utmQueryParams}`,
+        pluginDoc: `https://www.pixelbin.io/docs/integrations/photoshop/upscale.media/?${utmQueryParams}`,
 
         apiDomain: "https://api.pixelbin.io",
     },
